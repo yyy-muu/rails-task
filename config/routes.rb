a7d300e013d0c1d843797036e2e44e9c1f097d0b
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-07-18 20:37:27 +0900

    Add users controller, fix github signin link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "top#index"
+  
+  devise_for :users, :controllers => {
+    :sessions      => "users/sessions",
+    :registrations => "users/registrations",
+    :passwords     => "users/passwords",
+    # :omniauth_callbacks => "users/omniauth_callbacks" 
+  }
+  resources :users, :only => [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
