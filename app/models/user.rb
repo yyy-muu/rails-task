be4aa709c88526af831dd567afff43fd3bd70318
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-07-21 21:59:38 +0900

    Fix profile page can edit withou current passowrd #3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
   # validates :profile_image
-  validates :self_introduction, length: { maximum: 500 }
+  validates :self_introduction, length: { maximum: 160 }
+
+
+  # 第2引数(*options)は廃止された
+  def update_without_current_password(params)
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    # params([:password])をviewのfieldから引数として受け取る
+    # 保存に失敗したら例外を返す
+    result = update!(params)
+    
+    # passwordとpassword_confirmationの値をnilにする
+    clean_up_passwords
+    result
+  end
 end
