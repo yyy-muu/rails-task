aaa7dda905a4556922d189e7ce5a8adac00760a4
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-08-01 10:51:28 +0900

    Add comment model #7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_25_160151) do
+ActiveRecord::Schema.define(version: 2022_08_01_004445) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.integer "user_id", null: false
+    t.integer "tweet_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tweet_id"], name: "index_comments_on_tweet_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string "text"
@@ -36,5 +46,7 @@ ActiveRecord::Schema.define(version: 2022_07_25_160151) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "tweets"
+  add_foreign_key "comments", "users"
   add_foreign_key "tweets", "users"
 end
