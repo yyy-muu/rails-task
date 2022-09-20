abf51af0397b7fe828c1c0037baec8c4d98a1180
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-09-20 23:24:39 +0900

    Add relationship model #16

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   has_one_attached :profile_image
 
   devise :database_authenticatable, :registerable,
