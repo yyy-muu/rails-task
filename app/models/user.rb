28061da36bbcc787c8aa64d92877066509fb3cfe
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-10-13 00:30:46 +0900

    Add follow function #16

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes
-  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
-  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   has_one_attached :profile_image
@@ -54,14 +54,14 @@ class User < ApplicationRecord
 
   # ユーザフォロー・フォロー解除・フォロー確認
   def follow(other_user)
-    self.following << other_user
+    following << other_user
   end
 
   def unfollow(other_user)
-    self.following.destroy(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
   end
 
   def following?(other_user)
-    self.following.include?(other_user)
+    following.include?(other_user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
   root "top#index"
-  
+
   devise_for :users, :controllers => {
     :sessions      => "users/sessions",
     :registrations => "users/registrations",
     :passwords     => "users/passwords",
     :omniauth_callbacks => "users/omniauth_callbacks"
   }
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    member do
+      get :following, :followers
+    end
+  end
+
+  resources :relationships, only: [:create, :destroy]
 
   resources :tweets, only: [:index, :new, :create, :show] do
     resources :comments, only: [:new, :create]
   end
-
-  resources :relationships, only: [:create, :destroy]
 
   post 'comments/:id/like', to: 'comments#like_comment', as: 'like_comment'
   post 'comments/:id/unlike', to: 'comments#unlike_comment', as: 'unlike_comment'
