10123440e1fae769c8ed548f22c18eab61b817ec
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-10-20 00:38:00 +0900

    Add Relationship model tests #18

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:other_user) { FactoryBot.create(:user, email: 'otheruser@test.com') }
+
+  describe 'following' do
+    # ユーザが他のユーザをフォローできる
+    it 'can follow by other user' do
+      user.follow(other_user)
+      expect(user.following?(other_user)).to be_truthy
+    end
+
+    # ユーザが他のユーザをフォロー解除できる
+    it 'can unfollow by other user' do
+      user.follow(other_user)
+      user.unfollow(other_user)
+      expect(user.following?(other_user)).to be_falsey
+    end
+  end
 end
