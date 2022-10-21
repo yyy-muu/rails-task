3b9d8259c52e9f39dff3c8c6aa0d90198f299d91
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-10-22 00:19:17 +0900

    Add tweets system tests #18

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'Tweets', type: :system do
+  before do
+    visit new_user_session_path
+    fill_in 'メールアドレス', with: 'testuser@test.com'
+    fill_in 'パスワード', with: 'testtest'
+    click_on 'ログイン'
+  end
 
+  it 'user can tweet' do
+    click_on 'つぶやく'
+    expect(page).to have_content '新しいツイート'
+    fill_in 'tweet_text', with: 'test tweet!!'
+    click_on 'ツイートする'
+    expect(page).to have_content 'ツイートを送信しました'
+  end
+
+  it 'user cannot tweet without no texts' do
+    click_on 'つぶやく'
+    expect(page).to have_content '新しいツイート'
+    fill_in 'tweet_text', with: ''
+    click_on 'ツイートする'
+    expect(page).to have_content '新しいツイート'
+  end
+
+  it 'user can cancel tweet' do
+    click_on 'つぶやく'
+    expect(page).to have_content '新しいツイート'
+    fill_in 'tweet_text', with: 'test tweet!!'
+    click_on 'やっぱりやめる'
+    expect(page).to have_content 'つぶやく'
+  end
 end
