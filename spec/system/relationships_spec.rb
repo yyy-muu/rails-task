4f6f6221c76583e30cbdbf1005e7d39c7e96ed16
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-10-24 01:05:24 +0900

    Add relation ships system tests #18

--- spec/system/relationships_spec.rb
+++ spec/system/relationships_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Relationships', type: :system do
+  before do
+    other_user = FactoryBot.create(:user, name: 'Other User', email: 'otheruser5@test.com')
+    visit new_user_session_path
+    fill_in 'メールアドレス', with: other_user.email
+    fill_in 'パスワード', with: other_user.password
+    click_on 'ログイン'
 
+    click_on 'つぶやく'
+    fill_in 'tweet_text', with: 'test tweet!!'
+    click_on 'ツイートする'
+    click_on 'ログアウト'
+
+    visit new_user_session_path
+    fill_in 'メールアドレス', with: 'testuser@test.com'
+    fill_in 'パスワード', with: 'testtest'
+    click_on 'ログイン'
+
+    fill_in 'keywords', with: 'test tweet!!'
+    click_button '検索'
+  end
+
+  # 他のユーザをフォローできる
+  it 'user can follow other user' do
+    click_link 'Other User'
+    click_button 'フォロー'
+    expect(page).to have_button 'フォロー解除'
+
+  # 他のユーザをフォロー解除できる
+    click_button 'フォロー解除'
+    expect(page).to have_button 'フォロー'
+  end
 end
